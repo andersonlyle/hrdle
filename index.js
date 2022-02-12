28b85ab71be88353e11073a298c063fe1b4fe088
Lyle <lyle@lcanderson.com>
2022-02-12 16:18:05 -0500

    update

--- index.js
+++ index.js
@@ -74,5 +74,8 @@ app.get('/check', (req, res) => {
     })
 })
 
-
-app.listen(PORT, () => console.log('Server running on port ' + PORT))
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 8000;
+}
+app.listen(port, () => console.log('Server running on port ' + port))
